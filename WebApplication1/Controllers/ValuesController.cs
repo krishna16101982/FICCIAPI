95cd5cc45fc4ef9d33669b13f28ee8cc328ffadf
Kumar Gaurav <krishna16101982@gmail.com>
2023-08-28 18:10:03 +0530

    my initial commit

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/values
+        [Route("get")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        [Route("get/{id}")]
+        // GET: api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
